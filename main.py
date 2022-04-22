12ce2a4e8bf7dfc5af83d9e2ccf9b414a3202031
Philip Chiu <philipch07@gmail.com>
2022-04-22 14:27:18 -0400

    fixed counting txs and graph is correct

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ import requests
 
 API_BASE = "http://127.0.0.1:12973"
 a = '\u2135'
-#GENESIS_TS = 1231006504
-#FIRST_BLOCK_TS = 1636383298
-#############################
-#MY_STARTING_TS = 1636416000#
-#############################
+# Just a heads up that if you need to clear the txnslist.csv file, set it to be:
+"""
+txn,timeutc
+
+"""
+# including the newline.
+
+# GENESIS_TS = 1231006504
+# FIRST_BLOCK_TS = 1636383298
+###############################
+# MY_STARTING_TS = 1636416000 #
+###############################
 
 # gets the transactions from the user's own full-node (see API_BASE)
 def getBlockTsTransactions(session, start, end):
@@ -22,7 +29,7 @@ def getBlockTsTransactions(session, start, end):
         for inBlock in allBlocks['blocks']:
             for block in inBlock:
                 for transaction in block['transactions']:
-                    if len(transaction['unsigned']['inputs']) > 0:
+                    if len(transaction['unsigned']['inputs']) > 0:  # this filters out block rewards, which have no inputs
                         txs += 1
     else:
         print(f"There was an error code. See error: {response.status_code}")
@@ -36,29 +43,29 @@ def tconv(a):
 
 def collect():
     print("Collecting data...")
-    # start where we leave off
+    # This logic helps us to continue where we left off.
     f = open("currTxnTime.txt", "r+")
-    # we store the txns and the time here
-    # NOTE: the first line of the .csv should be "txn,timeutc". This is important for parsing the data from the csv.
-    # I won't be automatically doing that in the code, so you should prepare the .csv file in that way.
+    # Storing the txns and the time in txnslist.csv
     vals = open("txnslist.csv", "a")
 
-    # gets the number of txns in the past hour and stores it in the csv
-    # also updates the time in the starting file currTxnTime.txt
+    # Gets the number of txns in the past hour and stores it in the csv
+    # Also updates the time in the starting file currTxnTime.txt
     # I use a try-except block to catch the ctrl+c so it terminates cleanly.
     # Also note the f.seek(0) are necessary to keep the file line pointer at the top of the file.
     try:
         while (int(f.read()) < tconv(datetime.now())):
             f.seek(0)
             currTime = int(f.read())
-            # creating a request session
+
             s = requests.Session()
             s.headers.update(header)
+
             # getting the number of transactions
             # returns -1 on error status code
             numTxs = getBlockTsTransactions(s, currTime - 3600, currTime)
             if (numTxs == -1):
                 return
+
             vals.write(f'{numTxs},{currTime}\n')
             print(f'{numTxs}, {datetime.fromtimestamp(currTime).strftime("%m/%d/%Y, %H")}')
             newTime = currTime + 3600
--- parseData.py
+++ parseData.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # Parses the data in the csv into dicts and stores them into another csv
 # Note that this won't continue off of anywhere, and will have to parse through txnslist.csv from the very beginning.
+# This shouldn't be much of an issue though, as this part runs much quicker than the first part.
 def parse():
     print("Parsing data...")
     # The format of hours and months is:
@@ -18,10 +19,11 @@ def parse():
     # }
     hours = {}
     # this `tot` variable was just to check how many total transactions have been made.
-    tot = 0
+    # tot = 0
 
     for i in range(0, 24):
-        hours[i] = {"totalTxns": 0, "count": 0, "average": 0}
+        #hours[i] = {"totalTxns": 0, "count": 0, "average": 0}
+        hours[i] = {"totalTxns": 0}
 
     with open ('txnslist.csv', newline = '\n') as c:
         r = csv.DictReader(c)
@@ -37,21 +39,22 @@ def parse():
             if date.hour == 0:
                 fdata.write(f'{date.strftime("%m/%d/%Y")},')
                 for i in range(1, 24):
-                    fdata.write(f'{hours[i]["average"]},')
-                fdata.write(f'{hours[0]["average"]}\n')
+                    # fdata.write(f'{hours[i]["average"]},')
+                    fdata.write(f'{hours[i]["totalTxns"]},')
+                # fdata.write(f'{hours[0]["average"]}\n')
+                fdata.write(f'{hours[0]["totalTxns"]}\n')
 
             hours[date.hour]["totalTxns"] += txn
-            tot += txn
-            hours[date.hour]["count"] += 1
-            hours[date.hour]["average"] = round(hours[date.hour]["totalTxns"] / hours[date.hour]["count"], 2)
+            # tot += txn
+            # hours[date.hour]["count"] += 1
+            # hours[date.hour]["average"] = round(hours[date.hour]["totalTxns"] / hours[date.hour]["count"], 2)
 
     print("Parsing finished!")
-    print(f'Total Transactions: {tot}')
+    # print(f'Total Transactions: {tot}')
 
 # Creates the .mp4 animated bar chart race
 def graph():
     print("Creating graph...")
-    # following example from here:
     # https://github.com/dexplo/bar_chart_race
     tot = 0
     df = pd.read_csv('fdata.csv', index_col = 'date', parse_dates=['date'])
@@ -62,25 +65,25 @@ def graph():
         orientation = 'h',
         sort = 'desc',
         n_bars = 24,
-        fixed_order = False,
+        fixed_order = ["0-1","1-2","2-3","3-4","4-5","5-6","6-7","7-8","8-9","9-10","10-11","11-12","12-13","13-14","14-15","15-16","16-17","17-18","18-19","19-20","20-21","21-22","22-23","23-0"],
         fixed_max = False,
         steps_per_period = 30,
         period_length = 500,
         end_period_pause = 10,
         interpolate_period = False,
-        period_label = {'x': .98, 'y': .3, 'ha': 'right', 'va': 'center'},
+        period_label = {'x': .98, 'y': .4, 'ha': 'right', 'va': 'center'},
         period_template = '%B %d, %Y',
-        period_summary_func = lambda v, r:{'x': .98, 'y': .2,
-                                        's': f'Avg txs/day: {v.sum():,.0f}',
-                                        'ha': 'right', 'size': 11},
+        period_summary_func = lambda v, r:{'x': .98, 'y': .3,
+                                        's': f'Total tx\'s: {v.sum():,.0f}',
+                                        'ha': 'right', 'size': 10},
         perpendicular_bar_func = None,
         colors = 'dark24',
-        title = 'Average Transactions Per Hour on the Alephium Blockchain',
+        title = 'Total Transactions Per Hour on the Alephium Blockchain (UTC-4)',
         bar_size = .95,
         bar_textposition = 'inside',
         bar_texttemplate='{x:,.0f}', 
         bar_label_font=7, 
-        tick_label_font=7, 
+        tick_label_font=8, 
         tick_template='{x:,.0f}',
         shared_fontdict=None, 
         scale='linear', 
